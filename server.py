71e1a553093a18f927b2d2f4c2f24c127039f65d
BoleBorson <corsonca23@mail.vmi.edu>
2023-02-28 19:27:02 -0500

    finished endpoints

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/postData", methods=["POST"])
+def postData():
+    print("data", request.json)
+    return "It worked!"
+
+@app.route("/getData", methods=["GET"])
+def getData():
+    person = {
+        "name": "Cole Corson",
+        "age": 21
+    }
+
+    return json.dumps(person)
